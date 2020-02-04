a315eb051dd7755c12e4fddd96574e6800759a65
Owen Green <gungwho@gmail.com>
2020-02-04 07:33:57 +0000

    Add rsync for deploying

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const postcss = require('gulp-postcss');
+const rsync = require('gulp-rsync'); 
 
 // process CSS
 gulp.task('css', function () {
@@ -21,3 +22,19 @@ gulp.task('default', gulp.parallel('watch'));
 
 // build task
 gulp.task('build', gulp.parallel('css'));
+
+//deployment
+gulp.task('deploy', function(){
+  return gulp.src('_site/**')
+    .pipe(rsync({
+      root: '_site/',
+      username: 'flucoma',
+      hostname: 'flucoma',
+      destination: '~/www',
+      archive: true, 
+      silent: false, 
+      compress: true, 
+      delete: false, 
+      chmod: 'u=rwX,Dg=rX,Do=rX,Fg=r,Fu=r'
+    }));
+});
