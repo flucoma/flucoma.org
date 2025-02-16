a5874672ad277f355a3df90f3b33601800454b9f
James Bradbury <github@jamesbradbury.net>
2025-02-16 19:47:33 +0800

    use arrow functions in gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,41 @@
-const gulp = require('gulp');
-const postcss = require('gulp-postcss');
-const rsync = require('gulp-rsync'); 
+const gulp = require("gulp");
+const postcss = require("gulp-postcss");
+const rsync = require("gulp-rsync");
 
 // process CSS
-gulp.task('css', function () {
-  return gulp.src('./_src/css/styles.css')
-    .pipe(postcss())
-    .pipe(gulp.dest('./css'));
+gulp.task("css", () => {
+    return gulp
+        .src("./_src/css/styles.css")
+        .pipe(postcss())
+        .pipe(gulp.dest("./css"));
 });
 
 // watching
-gulp.task('watch:css', function() {
-  return gulp.watch(['./_src/css/**/*.css'],
-  gulp.series('css'));
+gulp.task("watch:css", () => {
+    return gulp.watch(["./_src/css/**/*.css"], gulp.series("css"));
 });
 
-gulp.task('watch', gulp.parallel('watch:css'));
+gulp.task("watch", gulp.parallel("watch:css"));
 
 // deafult task
-gulp.task('default', gulp.parallel('watch'));
+gulp.task("default", gulp.parallel("watch"));
 
 // build task
-gulp.task('build', gulp.parallel('css'));
+gulp.task("build", gulp.parallel("css"));
 
 //deployment
-gulp.task('deploy', function(){
-  return gulp.src('_site/**')
-    .pipe(rsync({
-      root: '_site/',
-      username: 'flucoma',
-      hostname: 'flucoma',
-      destination: '~/www',
-      archive: true, 
-      silent: false, 
-      compress: true, 
-      delete: false, 
-      chmod: 'u=rwX,Dg=rX,Do=rX,Fg=r,Fu=r'
-    }));
+gulp.task("deploy", () => {
+    return gulp.src("_site/**").pipe(
+        rsync({
+            root: "_site/",
+            username: "flucoma",
+            hostname: "flucoma",
+            destination: "~/www",
+            archive: true,
+            silent: false,
+            compress: true,
+            delete: false,
+            chmod: "u=rwX,Dg=rX,Do=rX,Fg=r,Fu=r",
+        }),
+    );
 });
